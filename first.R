52eeec8feb3e094cf8b2a0300598947c3159f6bc
오서현 <48465715+dlsanf2000@users.noreply.github.com>
2019-04-10 14:16:51 +0900

    Add files via upload

---
+++ first.R
@@ -0,0 +1,188 @@
+a <- 1
+a
+
+b <- 2
+b
+
+c<-3
+c
+d <- 3.5
+d
+
+a+b
+
+a+b+c
+
+4/b
+
+5*b
+
+(4/b)+5
+
+var1 <- c(1, 2, 5, 7, 8)
+var1
+
+var2 <- c(1:5)
+var2
+
+var3 <- seq(1,5)
+var3
+
+var4 <- seq(1, 10, by = 2)
+var4
+
+var5 <- seq(1, 10, by = 3)
+var5
+
+var2_1 <- c(1,5, by = 2)
+
+var2_1
+
+var1 
+
+var1+2
+
+var1
+
+
+var2
+
+var1 + var2
+
+exam <- read.csv("csv_exam.csv")
+exam
+
+head(exam)
+
+head (exam, 10)
+
+
+tail(exam)
+
+tail(exam, mpg)
+
+
+View(exam)
+
+dim(exam)
+
+str(exam)
+
+
+summary(exam())
+
+summary(exam)
+
+Median(exam)
+
+Mean(exam)
+
+mean(exam)
+
+install.packages("ggplot2")
+
+
+mpg <- as.data.frame(ggplot2::mpg)
+head(mpg)
+
+tail(mpg)
+View(mpg)
+
+
+dim(mpg)
+
+str1<-a
+str1
+
+str2 <-ab
+
+str3<-c
+str3
+
+
+str2<-d
+str2
+
+
+str9 <- q
+str9
+
+str8 <- text
+str9
+str8
+
+str8
+
+
+library(ggplot2)
+
+x <- c("a", "a", "b", "c")
+x
+
+qplot(x)
+
+qplot(data = mpg, x= hwy)
+
+qplot(data = mpg, x = cty)
+
+qplot(data = mpg, x = drv, y = hwy)
+
+var1
+
+var2
+
+var1+var2
+
+str <- "a"
+str1
+str
+
+
+str<-"a"
+str
+
+
+str2 <- "text"
+str2
+
+
+str3<-"Hello World"
+str3
+
+
+str4 <- c("a", "b", "c")
+str4
+
+str5 <- c("Hello", "World", "is", "good")
+str5
+
+
+str*str2
+
+x <- c(1,2,3)
+x
+
+mean(x)
+
+max(x)
+
+min(x)
+
+str5
+
+paste(str5)
+
+paste(str5, collapse = ",")
+
+paste(str5)
+
+paste(str5, collapse = "-")
+
+paste(str5, collpase=" ")
+
+x_mean <- mean(x)
+x_mean
+
+str5_paste <- paste(str5, collapse = " ")
+str5_paste
+
